7f7e40ede2a4b3ea1563814eb81c1c8a0b733957
DarielRdriguez <Darielrwork@gmail.com>
2023-07-24 17:27:53 -0400

    viewBerryFlavor built-it function
    Return list of berries flavor names

--- __init__.py
+++ __init__.py
@@ -2,3 +2,6 @@
 from poke.pokeAPI import * 
 from poke.Berries import *
 
+# print(getBerries('cheri'))
+print(viewBerryFlavor(1))
+
--- poke/Berries.py
+++ poke/Berries.py
@@ -1,17 +1,34 @@
 from requests import  HTTPError, get
-from json import loads, dumps
+from collections import OrderedDict
 
-__URL = "https://pokeapi.co/api/v2/berry"
+__URL = "https://pokeapi.co/api/v2/"
 
 #TODO: Berries module
 
+#NOTE: Function to return some basic berry info
+
 def getBerries(name:str):
     try:
-        response = get(f"{__URL}/{name}") 
+        response = get(f"{__URL}/berry/{name}") 
         if response.status_code == 200:
-            basicInfo = response.json()
-            basicTxt = "Berry Name:{}, \nSize: {}".format(*list(map(basicInfo.get, ('name','size'))))
-            return basicTxt
+            basicJson = response.json()
+            basicInfo = {key:basicJson[key] for key in basicJson.keys() & {'name', 'size', 'growth_time', 'soil_dryness'}}
+            return basicInfo
+    except HTTPError as e:
+        return e
+
+#NOTE: Function to see berries Flavor (return berries flavor name)
+
+def viewBerryFlavor(nm:int):
+    try:
+        if not nm in list(range(1,5)) :
+            raise Exception ('Not found berry flavor')
+        else:
+            response = get(f"{__URL}/berry-flavor/{nm}")
+            if response.status_code == 200:
+                brryJson = response.json()
+                brryName = [x.get('berry')['name'] for x in brryJson['berries']]
+                return sorted(brryName) 
     except HTTPError as e:
         return e
 
